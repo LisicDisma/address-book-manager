56081376981f424002bc5b08f8bd4ba163cfd7f4
LisicDisma <kotukkc@gmail.com>
2025-12-12 10:58:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,10 @@ setup(
     author='Шабанова Ксения Алексеевна',
     author_email='kotukkc@gmail.com', 
     description='''Простой, полностью автономный пакет на Python для управления контактами и адресами, включающий классы Contact, Address и AddressBook.
-    ## Address Book Manager (address-book-manager)
-    
-    Этот пакет предоставляет основные классы для работы с адресной книгой:
-    * `Address`: хранение улицы, города и индекса.
-    * `Contact`: хранение имени, телефона, email и объекта `Address`.
-    * `AddressBook`: контейнер для контактов с методами для добавления, удаления, поиска, редактирования и экспорта в JSON.
-    
-    Пакет также включает исполняемый файл `address-book-cli` для запуска демонстрации из командной строки.
     ''', 
     include_package_data=True,
     install_requires=[],
 
 )
+
 
