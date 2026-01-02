f3460e8705bde63bd75f14f17066d4dbf093a39b
LisicDisma <kotukkc@gmail.com>
2026-01-03 01:16:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name='address-book-manager', 
     version='0.1.0', 
-    packages=find_packages("."),
-    scripts=["bin/address-book-cli"], # Расположение главного исполняемого файла.
+    packages=find_packages("."), 
+    scripts=["bin/address-book-cli.py"]# Расположение главного исполняемого файла.
     url='https://github.com/LisicDisma/address-book-manager', # Адрес репозитория с вашей курсовой работой.
     license='Apache-2.0',
     author='Шабанова Ксения Алексеевна',
@@ -13,4 +13,5 @@ setup(
     description='Простой, полностью автономный пакет на Python для управления контактами и адресами, включающий классы Contact, Address и AddressBook.', 
     include_package_data=True,
     install_requires=[],
+
 )
